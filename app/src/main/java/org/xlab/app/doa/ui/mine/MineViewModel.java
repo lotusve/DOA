6cec522d3593487040a686ed8aa3912a0bfb117e
lotusve <lotusve@163.com>
2020-09-23 10:13:19 +0800

    add mine

---
+++ app/src/main/java/org/xlab/app/doa/ui/mine/MineViewModel.java
@@ -0,0 +1,19 @@
+package org.xlab.app.doa.ui.mine;
+
+import androidx.lifecycle.LiveData;
+import androidx.lifecycle.MutableLiveData;
+import androidx.lifecycle.ViewModel;
+
+public class MineViewModel extends ViewModel {
+
+    private MutableLiveData<String> mText;
+
+    public MineViewModel() {
+        mText = new MutableLiveData<>();
+        mText.setValue("This is mine fragment");
+    }
+
+    public LiveData<String> getText() {
+        return mText;
+    }
+}
